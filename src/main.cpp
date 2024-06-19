f245ba4122e1a5e886d9122c022a91b83ff870ac
vardhanbang <132768477+vardhanbang@users.noreply.github.com>
2024-06-19 20:52:53 +0530

    deleted shaders and linked vertattrib to VBO

--- src/main.cpp
+++ src/main.cpp
@@ -83,6 +83,11 @@ int main() {
         std::cout << "ERROR SHADER PROGRAM LINKING FAILED\n" << infoLog << std::endl;
     }
 
+    //delete shaders after linking
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
     //triangle vertices
     float vertices[] = {
         -0.5, -0.5f, 0.0f,
@@ -96,6 +101,8 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     //render loop 
     while(!glfwWindowShouldClose(window)) {
